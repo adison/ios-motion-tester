72e610e6c20be29711896b9e4ce34de1f4f4d824
adison <adison.wu@gmail.com>
2014-08-20 09:13:38 +0800

    移除旧码

--- motion-detect/ViewController.m
+++ motion-detect/ViewController.m
@@ -38,80 +38,6 @@
                                 aData.rotationRate.y,
                                 aData.rotationRate.z);
     }];
-//    [detector getDeviceMotion:^(CMDeviceMotion *aData) {
-//        lblMotion.text = NSPRINTF(@"磁力？\nx :%+2.f\ny :%+2.f\nz :%+2.f",
-//                               aData.m.x,
-//                               aData.acceleration.y,
-//                               aData.acceleration.z);
-//    }];
-    
-}
-
--(void)runDetect {
-    if(manager == nil) {
-        manager = [[CMMotionManager alloc] init];
-    }
-    // 加速度
-//    if (!manager.isAccelerometerAvailable){
-//        lblAcc.text = @"加速度計不存在";
-//        return;
-//    }
-//    else {
-//        if (!manager.isAccelerometerActive ) {
-//            lblAcc.text = @"加速度計未啟動";
-//        }
-//        // 取得資料顯示
-//        NSOperationQueue *op1 = [[NSOperationQueue alloc] init];
-//        manager.accelerometerUpdateInterval = .2f;
-//        [manager
-//         startAccelerometerUpdatesToQueue:op1
-//         withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
-//             if (error) {
-//                 [manager stopAccelerometerUpdates];
-//                 strAcc = NSPRINTF(@"加速度計有錯誤 %@", error);
-//                 [lblAcc performSelectorOnMainThread:@selector(setText:) withObject:strAcc waitUntilDone:YES];
-//                 return;
-//             }
-//             strAcc = NSPRINTF(@"加速度計\nx: %+.2f\ny:%+.2f \n z:%+.2f",
-//                                    accelerometerData.acceleration.x,
-//                                    accelerometerData.acceleration.y,
-//                                    accelerometerData.acceleration.z);
-//             [lblAcc performSelectorOnMainThread:@selector(setText:) withObject:strAcc waitUntilDone:YES];
-//         }];
-//    }
-    
-    
-    // 陀螺儀
-    // 加速度
-//    if (!manager.isGyroAvailable){
-//        lblAcc.text = @"陀螺儀不存在";
-//        return;
-//    }
-//    else {
-//        if (!manager.isGyroActive ) {
-//            lblAcc.text = @"陀螺儀未啟動";
-//        }
-//        // 取得資料顯示
-//        NSOperationQueue *op2 = [[NSOperationQueue alloc] init];
-//        manager.gyroUpdateInterval = .2f;
-//        [manager
-//         startGyroUpdatesToQueue:op2
-//         withHandler:^(CMGyroData *gyroData, NSError *error) {
-//             if (error) {
-//                 [lblGeny performSelectorOnMainThread:@selector(setText:)
-//                                           withObject:NSPRINTF(@"陀螺儀錯誤 %@", error)
-//                                        waitUntilDone:YES];
-//                 [manager stopGyroUpdates];
-//                 return ;
-//             }
-//             [lblGeny performSelectorOnMainThread:@selector(setText:)
-//                                       withObject:NSPRINTF(@"陀螺儀\nx: %+.2f\ny: %+.2f\nz: %+.2f",
-//                                                           gyroData.rotationRate.x *1000,
-//                                                           gyroData.rotationRate.y*1000,
-//                                                           gyroData.rotationRate.z*1000)
-//                                    waitUntilDone:YES];
-//         }];
-//    }
 }
 
 - (void)viewDidLoad {
@@ -120,7 +46,6 @@
     lblGeny.numberOfLines = 5;
     lblMag.numberOfLines = 5;
     lblMotion.numberOfLines = 5;
-//    [self runDetect];
     
     [[MotionDetector sharedManager] start];
     detector = [MotionDetector sharedManager];
